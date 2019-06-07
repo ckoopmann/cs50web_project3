2b0bf7474eb47881fd1e6ead4f8b018111ffd942
Christian Koopmann <koopmannc@tipico.com>
2019-06-07 12:14:59 +0200

    Fix name prompt

--- static/index.js
+++ static/index.js
@@ -1,9 +1,10 @@
-// Set starting value of name to 0
-if (!localStorage.getItem('name'))
-    localStorage.setItem('name', prompt("Enter your Name","dickhead"));
+
 
 // Load current value of  name
 document.addEventListener('DOMContentLoaded', () => {
+  // Set starting value of name to 0
+  if (!localStorage.getItem('name'))
+      localStorage.setItem('name', prompt("Enter your Name","dickhead"));
 
   const name = localStorage.getItem('name');
   document.querySelector('#heading').innerHTML = name;
