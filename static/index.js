55fc3ac05a37067d8a668ec3d1fca76c8edfd0e1
Christian Koopmann <koopmannc@tipico.com>
2019-06-07 11:59:08 +0200

    Add form for channel creation

--- static/index.js
+++ static/index.js
@@ -4,11 +4,6 @@ if (!localStorage.getItem('name'))
 
 // Load current value of  name
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#heading').innerHTML = localStorage.getItem('name');
-
-    document.querySelector('#form').onsubmit = function() {
-        const name = document.querySelector('#name').value;
-        localStorage.setItem('name', name);
-        document.querySelector('#heading').innerHTML = name;
-    };
+    const name = localStorage.getItem('name');
+    document.querySelector('#heading').innerHTML = name;
 });
