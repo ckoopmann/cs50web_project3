cde0217a4a83ff0ce1199111ff66dd02f0ef7094
Christian Koopmann <koopmannc@tipico.com>
2019-06-07 12:13:23 +0200

    Add client side channel name emission

--- static/index.js
+++ static/index.js
@@ -4,6 +4,22 @@ if (!localStorage.getItem('name'))
 
 // Load current value of  name
 document.addEventListener('DOMContentLoaded', () => {
-    const name = localStorage.getItem('name');
-    document.querySelector('#heading').innerHTML = name;
+
+  const name = localStorage.getItem('name');
+  document.querySelector('#heading').innerHTML = name;
+
+
+    // Connect to websocket
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+  // When connected, configure buttons
+  socket.on('connect', () => {
+
+    // Each button should emit a "submit vote" event
+    document.querySelector('#channel').onsubmit = () => {
+        var channelname = document.querySelector('#channelname').value;
+        socket.emit('create channel', {'chanelname': channelname});
+      };
+    });
+  });
 });
